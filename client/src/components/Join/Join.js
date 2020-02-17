376ea6a3511e8b5b733ec0cc1e37282c411c7797
Scott Nelsen <snelsen0106@gmail.com>
2020-02-16 21:47:07 -0600

    added functionality to Join.js

---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    return (
+        <div className="joinContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div>
+                    <input placehoder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+                </div>
+                <div>
+                    <input placehoder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+                </div>
+                <Link onClick ={event => (!name || !room) ?event.preventDefault() : null}to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+                </div>
+            </div>
+        
+    )
+}
+
+export default Join;
