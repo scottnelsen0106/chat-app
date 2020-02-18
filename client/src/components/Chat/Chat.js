cefbd6d2082f865b9967372608503853c24144e2
Scott Nelsen <snelsen0106@gmail.com>
2020-02-17 22:09:23 -0600

    got the console logging on Chat.js working

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,6 +7,9 @@ let socket;
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
     const ENDPOINT = 'localhost:5000';
 
     useEffect(()=> {
@@ -17,10 +20,40 @@ const Chat = ({ location }) => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', { name, room });
+        socket.emit('join', { name, room }, () => {
+            
+        });
+        return () => {
+            socket.emit('disconnect')
+            socket.off();
+        }
     }, [ENDPOINT, location.search])
+
+useEffect(() => {
+    socket.on('message', (message) => {
+        setMessages( [...messages, message] )
+    })
+}, [messages]);
+
+const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+        socket.emit('sendMessage', message, () => setMessage(''));
+    }
+}
+
+console.log(message, messages);
+
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <input value={message} onChange={(event) => setMessage(event.target.value)}  
+                onKeyPress = {event => event.key === 'Enter' ? sendMessage(event): null}
+                />
+
+            </div>
+        </div>
     )
 }
 
